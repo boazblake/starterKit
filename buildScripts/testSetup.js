a06fbb1c75908967c45dafe4dd8cb59b9f4a4577
Boaz Blake <boazblake@gmail.com>
2016-11-12 12:50:01 -0600

    all working

---
+++ buildScripts/testSetup.js
@@ -0,0 +1,7 @@
+//This file is not transpiled so must use CommonJS and ES5
+
+//register babel to transpile before our tests run
+require('babel-register')();
+
+//Disable webpack features that mocha doesnt understand
+require.extensions['.css'] = function(){};
---
+++ src/index.test.js
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+
+
+describe( 'My first test', () => {
+  it('should pass', () => {
+    expect(true).to.equal(true);
+  });
+});
